fd0486c8e94646a79f8c7c109f280cceccf28dd9
nick <nikhilsingh892710@gmail.com>
2021-10-09 12:23:14 +0530

    documenatation completed

--- functions.php
+++ functions.php
@@ -18,16 +18,6 @@ function verifysessionadmin(){
         return false;
     }
 }
-function randomPassword() {
-    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-    $pass = array(); //remember to declare $pass as an array
-    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-    for ($i = 0; $i < 8; $i++) {
-        $n = rand(0, $alphaLength);
-        $pass[] = $alphabet[$n];
-    }
-    return implode($pass); //turn the array into a string
-}
 function Redirect($url, $permanent = false)
 {
 /*     if (headers_sent() === false)
